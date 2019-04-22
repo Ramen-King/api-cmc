24c702af5221d379dda5fd84895809d253a248fa
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-04-22 11:20:46 -0400

    added line 30 for imgurl change

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 window.onload = init;
 
-function init(){
-    const submitRequest = document.querySelector('button');
-    submitRequest.addEventListener('click', request);
+function init() {
+  const submitRequest = document.querySelector(".card-area");
+  submitRequest.addEventListener("click", request);
 }
-function request(event){
-    event.preventDefault();
-   
-    sendRequest();
+function request(event) {
+  event.preventDefault();
+
+  sendRequest();
 }
 
-function sendRequest(){  
-    const xhr = new XMLHttpRequest()
-    const url = `https://api.magicthegathering.io/v1/cards/`;
-    xhr.open("GET", url);
-    xhr.onload = handleData;
-    xhr.send();
+function sendRequest() {
+  const xhr = new XMLHttpRequest();
+  const url = `https://api.magicthegathering.io/v1/cards/`;
+  xhr.open("GET", url);
+  xhr.onload = handleData;
+  xhr.send();
 }
+function handleData(event) {
+  const deck = JSON.parse(event.target.responseText);
+  const { cards } = deck;
 
-function handleData(event){
-    const deck = JSON.parse(event.target.responseText);
-    const { cards } = deck
-    console.log(deck);
-    console.log(cards[4].imageUrl)
+  //get a random card
+  const cardNumber = Math.floor(Math.random() * 100);
+  console.log(deck);
+
+  const cardSelected = cards[cardNumber].imageUrl;
+//   newCard(cardSelected);
+document.querySelector(".card-area").src = `${cardSelected}`;
+
 }
