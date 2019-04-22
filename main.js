3210a00a57a943e0a3e2614a132becddca3d2288
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-04-22 13:17:11 -0400

    added append stats

--- main.js
+++ main.js
@@ -20,13 +20,46 @@ function sendRequest() {
 function handleData(event) {
   const deck = JSON.parse(event.target.responseText);
   const { cards } = deck;
+  console.log(cards);
 
   //get a random card
   const cardNumber = Math.floor(Math.random() * 100);
   console.log(deck);
+  const cardSelected = cards[cardNumber];
 
-  const cardSelected = cards[cardNumber].imageUrl;
-//   newCard(cardSelected);
-document.querySelector(".card-area").src = `${cardSelected}`;
+  const cardSelectedImg = cardSelected.imageUrl;
+  //   newCard(cardSelected);
+  document.querySelector(".card-area").src = cardSelectedImg;
+  console.log(cardSelected);
+
+  listStats(cardSelected);
+}
+
+function listStats(cardSelected) {
+ 
+  const statsWanted = [cardSelected.name, cardSelected.type, cardSelected.rarity];
+  const ul = document.querySelector('.stats');
+  for (let i = 0; i < statsWanted.length; i++){
+    const li = document.createElement('li');
+      li.innerText = statsWanted[i];
+      ul.appendChild(li);
+  }
+  while (request) {
+    ul.removeChild()
+  }
+
+  
+  
+    // statNodes.innerText = cardSelected.statsWanted[i];
+    // ul.appendChild(statNodes);
+    // 
+    // statNodes.innerText = cardSelected.colorIdentity;
+    // ul.appendChild(statNodes);
+    // 
+    // statNodes.innerText = cardSelected.types;
+    // ul.appendChild(statNodes);
+    // 
+    // statNodes.innerText = cardSelected.rarity;
+    // ul.appendChild(statNodes);
 
 }
